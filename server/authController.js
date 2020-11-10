3f104ae8fbd973a2d8382dfeb4ec665d3f6ec5b2
Jabinator1 <jabinator1@gmail.com>
2020-11-10 11:35:12 -0700

    finished full authorization for users

--- server/authController.js
+++ server/authController.js
@@ -1,14 +1,55 @@
+const bcrypt = require('bcrypt')
+
 module.exports = {
-    register: (req, res) => {
+    register: async (req, res) => {
+        const db = req.app.get('db')
+        const { email, username, password } = req.body
+        const user = await db.check_user(email, username, password)
 
+        if (user[0]) {
+            return res.status(409).send("User already exists")
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt)
+        const [newUser] = await db.add_user([username, email, hash])
+
+        req.session.user = {
+            userId: newUser.user_id,
+            email: newUser.email,
+            username: newUser.username
+        }
+
+        res.status(200).send(req.session.user)
     },
-    login: (req, res) => {
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {email, password} = req.body
+        const [user] = await db.check_user(email)
+        if (!user) {
+            return res.status(409).send("Incorrect credentials")
+        }
 
+        const authenticated = bcrypt.compareSync(password, user.password)
+
+        if (authenticated) {
+            const {user_id, email, username} = user
+            req.session.user = {
+                userId: user_id,
+                email: email,
+                username: username
+            }
+            res.status(200).send(req.session.user)
+        } else {
+            res.status(401).send("Incorrect credentials")
+        }
     },
     logout: (req, res) => {
-
+        req.session.destroy()
+        res.sendStatus(200)
     },
     getUser: (req, res) => {
-        
+        req.session.user ? res.status(200).send(req.session.userId)
+        : res.sendStatus(404)
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
+const session = require('express-session')
+
 // require session
-const auth = require('./authController');
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const {register, login, logout, getUser} = require('./authController');
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 // Top level middleware
 app.use(express.json());
+app.use(session({
+    //# don't save if no changes are made
+    resave: false,
+
+    //# even if there's no new info, we still want to make a session
+    saveUninitialized: true,
+
+    //# basically a cookie encryption
+    secret: SESSION_SECRET,
+
+    //# how long the cookie will last (usually a week or two)
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+}))
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -20,5 +35,10 @@ massive({
 }).catch( err => console.log(err));
 
 // Enpoints
+
+app.post("/auth/register", register)
+app.post("/auth/login", login)
+app.post("/auth/logout", logout)
+app.get("/auth/get-user", getUser)
 
 app.listen(SERVER_PORT, () => console.log(`Connected to port ${SERVER_PORT}⛵⚓`))
